a014e955262c19b3099c2a73aa3f235a3d2865eb
fabbe50 <fabbe50@gmail.com>
2018-01-15 22:35:26 +0100

    Items

--- src/main/java/com/resimulators/simukraft/common/items/base/ItemFoodBase.java
+++ src/main/java/com/resimulators/simukraft/common/items/base/ItemFoodBase.java
@@ -2,13 +2,14 @@ package com.resimulators.simukraft.common.items.base;
 
 import com.resimulators.simukraft.Reference;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
 
 /**
- * Created by fabbe on 06/01/2018 - 4:51 AM.
+ * Created by fabbe on 06/01/2018 - 6:16 AM.
  */
-public class ItemBase extends Item {
-    public ItemBase(String name, CreativeTabs tab) {
+public class ItemFoodBase extends ItemFood {
+    public ItemFoodBase(String name, CreativeTabs tab, int amount, float saturation, boolean isWolfFood) {
+        super(amount, saturation, isWolfFood);
         this.setName(name);
         this.setCreativeTab(tab);
     }
--- src/main/java/com/resimulators/simukraft/init/ModItems.java
+++ src/main/java/com/resimulators/simukraft/init/ModItems.java
@@ -2,6 +2,7 @@ package com.resimulators.simukraft.init;
 
 import com.resimulators.simukraft.SimUTab;
 import com.resimulators.simukraft.common.items.ItemGranules;
+import com.resimulators.simukraft.common.items.base.ItemFoodBase;
 import com.resimulators.simukraft.registry.RegistryHandler;
 import net.minecraft.item.Item;
 
@@ -10,8 +11,16 @@ import net.minecraft.item.Item;
  */
 public class ModItems {
     public static final Item GRANULES = new ItemGranules("granules", SimUTab.SUTab);
+    public static final Item BURGER = new ItemFoodBase("burger", SimUTab.SUTab, 8, 8, false);
+    public static final Item CHEESE = new ItemFoodBase("cheese", SimUTab.SUTab, 4, 4, false);
+    public static final Item CHEESEBURGER = new ItemFoodBase("cheeseburger", SimUTab.SUTab, 14, 10, false);
+    public static final Item FRIES = new ItemFoodBase("fries", SimUTab.SUTab, 5, 6, false);
 
     public static void init() {
         RegistryHandler.registerItem(GRANULES, "granules-copper", "granules-gold", "granules-iron", "granules-tin");
+        RegistryHandler.registerItem(BURGER);
+        RegistryHandler.registerItem(CHEESE);
+        RegistryHandler.registerItem(CHEESEBURGER);
+        RegistryHandler.registerItem(FRIES);
     }
 }
