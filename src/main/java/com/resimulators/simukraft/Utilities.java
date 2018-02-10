80c98e025467a68d93ed277f7e46ab711e4b1168
fabbe50 <fabbe50@gmail.com>
2018-02-10 05:19:41 +0100

    Added custom destroyBlock method

--- src/main/java/com/resimulators/simukraft/Utilities.java
+++ src/main/java/com/resimulators/simukraft/Utilities.java
@@ -1,7 +1,14 @@
 package com.resimulators.simukraft;
 
 import com.mojang.realmsclient.gui.ChatFormatting;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 /**
  * Created by fabbe on 04/02/2018 - 5:32 PM.
@@ -24,5 +31,24 @@ public class Utilities {
             builder.append(aStr.substring(0, 1).toUpperCase()).append(aStr.substring(1)).append(" ");
         }
         return builder.toString();
+    }
+
+    public static boolean destroyBlock(World world, BlockPos pos, boolean dropBlock, boolean dropTrueBlock, boolean soundParticles) {
+        IBlockState iblockstate = world.getBlockState(pos);
+        Block block = iblockstate.getBlock();
+
+        if (block.isAir(iblockstate, world, pos))
+            return false;
+        else {
+            if (soundParticles)
+                world.playEvent(2001, pos, Block.getStateId(iblockstate));
+
+            if (dropBlock && !dropTrueBlock)
+                block.dropBlockAsItem(world, pos, iblockstate, 0);
+            else if (dropTrueBlock)
+                world.spawnEntity(new EntityItem(world, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(Item.getItemFromBlock(block))));
+
+            return world.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
+        }
     }
 }
