412b2990e38694e772004614b7544a38bba8a517
fabbe50 <fabbe50@gmail.com>
2018-02-17 22:59:33 +0100

    Button Removal Sorted
    I know it's removing them as soon as you click... but really, add some fancy logic inbefore the removal.

--- src/main/java/com/resimulators/simukraft/client/gui/GuiMiner.java
+++ src/main/java/com/resimulators/simukraft/client/gui/GuiMiner.java
@@ -2,8 +2,11 @@ package com.resimulators.simukraft.client.gui;
 
 import com.resimulators.simukraft.common.entity.entitysim.EntitySim;
 import com.resimulators.simukraft.common.entity.entitysim.SimToHire;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraftforge.fml.client.GuiScrollingList;
 
 import java.awt.*;
 import java.io.IOException;
@@ -19,11 +22,15 @@ public class GuiMiner extends GuiScreen {
     private GuiButton button1;
     private GuiButton button2;
     private List<EntitySim> sims;
+    private int mouseX;
+    private int mouseY;
 
     @Override
     public void drawScreen(int mouseX, int mouseY,float partialTicks) {
         drawDefaultBackground();
         drawString(mc.fontRenderer, "Miner", (width / 2) - (buttonWidth / 3), height / 4 - 10, Color.WHITE.getRGB());
+        this.mouseX = mouseX;
+        this.mouseY = mouseY;
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
 
@@ -31,15 +38,15 @@ public class GuiMiner extends GuiScreen {
     public void initGui() {
         sims = SimToHire.sims;
         buttonList.clear();
+        buttonList.add(button1 = new GuiButton(0, width / 2 - buttonWidth / 2, height - 80, "Hire"));
+        buttonList.add(button2 = new GuiButton(1, width / 2 - buttonWidth / 2, height - 50, "Cancel"));
         for (int i = 2; i < sims.size(); i++) {
             String name = sims.get(i).getName();
-            buttonList.add(new GuiButton(i, i * xOffset, yOffset, name));
+            buttonList.add(new GuiButton(i, i * xOffset, yOffset + 10, name));
             if (i > 5) {
-                yOffset = 40;
+                yOffset = 30;
             }
         }
-        buttonList.add(button1 = new GuiButton(0, width / 2 - buttonWidth / 2, height / 4, "Hire"));
-        buttonList.add(button2 = new GuiButton(1, width / 2 - buttonWidth / 2, height / 4 + (4 * yOffset), "Cancel"));
 
         super.initGui();
     }
@@ -51,6 +58,10 @@ public class GuiMiner extends GuiScreen {
                 status = "hiring";
                 updateButtons();
                 break;
+            case 1:
+                break;
+            default:
+                disableButton(button.id);
         }
         super.actionPerformed(button);
     }
@@ -62,9 +73,46 @@ public class GuiMiner extends GuiScreen {
         }
     }
 
+    private boolean disableButton(int id) {
+        buttonList.get(id).enabled = false;
+        buttonList.get(id).visible = false;
+        return buttonList.get(id).enabled && buttonList.get(id).visible;
+    }
+
     @Override
     public boolean doesGuiPauseGame() {
         return false;
+    }
+
+    public class ButtonList extends GuiScrollingList {
+        public ButtonList(Minecraft client, int width, int height, int top, int bottom, int left, int entryHeight, int screenWidth, int screenHeight) {
+            super(client, width, height, top, bottom, left, entryHeight, screenWidth, screenHeight);
+        }
+
+        @Override
+        protected int getSize() {
+            return 20;
+        }
+
+        @Override
+        protected void elementClicked(int index, boolean doubleClick) {
+
+        }
+
+        @Override
+        protected boolean isSelected(int index) {
+            return false;
+        }
+
+        @Override
+        protected void drawBackground() {
+
+        }
+
+        @Override
+        protected void drawSlot(int slotIdx, int entryRight, int slotTop, int slotBuffer, Tessellator tess) {
+
+        }
     }
 }
 
