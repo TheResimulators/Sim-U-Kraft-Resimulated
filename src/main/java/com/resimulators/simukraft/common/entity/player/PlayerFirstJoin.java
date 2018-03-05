32db3362def50d28b1e7adf52573280e71d129dd
aidie8 <aidenvanzuilen@gmail.com>
2018-03-06 07:10:08 +1300

    Changes to for Gui Start

--- src/main/java/com/resimulators/simukraft/common/entity/player/PlayerFirstJoin.java
+++ src/main/java/com/resimulators/simukraft/common/entity/player/PlayerFirstJoin.java
@@ -2,9 +2,12 @@ package com.resimulators.simukraft.common.entity.player;
 
 import com.resimulators.simukraft.GuiHandler;
 import com.resimulators.simukraft.SimUKraft;
+import ibxm.Player;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -13,11 +16,12 @@ public class PlayerFirstJoin {
     private boolean isLoaded = false;
     @SubscribeEvent
     @SideOnly(Side.CLIENT)
-    public void OnTick(TickEvent PlayerTickEvent, World world) {
+    public void OnTick(PlayerEvent.PlayerLoggedInEvent event) {
+        World world = event.
         if (!world.isRemote && !isLoaded) {
             isLoaded = true;
             if (!world.isRemote) {
-                Minecraft.getMinecraft().player.openGui(SimUKraft.instance, GuiHandler.GUI_START, world, 0, 0, 0);
+                Minecraft.getMinecraft().player.openGui(SimUKraft.instance, GuiHandler.GUI_START, world, 1, 1, 1);
             }
         }
 
