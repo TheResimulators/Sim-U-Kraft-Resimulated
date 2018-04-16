d8bc463c7da4a71ffccff1d9549aea30461752b7
aidie8 <aidenvanzuilen@gmail.com>
2018-04-16 22:00:09 +1200

    semi-fixed the nbt saving

--- src/main/java/com/resimulators/simukraft/common/entity/entitysim/SimToHire.java
+++ src/main/java/com/resimulators/simukraft/common/entity/entitysim/SimToHire.java
@@ -1,36 +1,23 @@
 package com.resimulators.simukraft.common.entity.entitysim;
 import com.resimulators.simukraft.network.PacketHandler;
 import com.resimulators.simukraft.network.Siminfo_packet;
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.*;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldServer;
-import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
-import net.minecraftforge.event.entity.living.BabyEntitySpawnEvent;
-import net.minecraftforge.event.entity.living.LivingSpawnEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
-import net.minecraftforge.fml.relauncher.SideOnly;
-import org.lwjgl.Sys;
-
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 
-import static net.minecraftforge.fml.relauncher.Side.CLIENT;
 
 public class SimToHire {
 
     public static List<EntitySim> unemployedsims = new ArrayList<>();
     public static List<EntitySim> totalsims = new ArrayList<>();
-
+    NBTTagList unemployes_sims = new NBTTagList();
+    NBTTagList total_sims = new NBTTagList();
     public static float getCredits() {
         //System.out.println(" getting credits that is equal to: " + credits);
         return credits;
@@ -39,9 +26,6 @@ public class SimToHire {
         credits = credit;
         //System.out.println(" set credits to" + credits);
         return credits;
-    }
-    public static UUID getSimID(){
-        return sim;
     }
     static UUID sim;
     static float credits = 10;
@@ -72,44 +56,44 @@ public class SimToHire {
     }
     @SubscribeEvent
     public void world_save(net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedOutEvent event){
+        World world = event.player.getEntityWorld();
+
+        if (!world.isRemote){
         System.out.println("save ");
-        compound.setInteger("total_sims",totalsims.size());
-        compound.setInteger("unemployed_sims",unemployedsims.size());
-        for(int i = 0;i< totalsims.size();i++){
-            UUID id = totalsims.get(i).getPersistentID();
+        for(int i = 0;i< unemployedsims.size();i++){
+            UUID id = unemployedsims.get(i).getPersistentID();
                 compound.setUniqueId("sim " + i,id);
+                unemployes_sims.appendTag(new NBTTagString(id.toString()));
                 System.out.println("added sim " + id + " to nbt");
 
             }
         for(int i = 0;i< totalsims.size();i++){
             UUID id = totalsims.get(i).getPersistentID();
             compound.setUniqueId("sims " + i,id);
-        }
-
+            total_sims.appendTag(new NBTTagString(id.toString()));
+        }}
         }
     @SubscribeEvent
     public void world_load(net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent event){
+        World world = event.player.getServer().getEntityWorld();
+        if (!world.isRemote){
         System.out.println("loading sims");
         MinecraftServer server = event.player.getEntityWorld().getMinecraftServer();
-        int total_sims = compound.getInteger("total_sims");
-        int unemployed_sims = compound.getInteger("unemployed_sims");
-        for (int i = 0;i<unemployed_sims;i++){
-            if (compound.hasKey("sim " + i)){
-                UUID sim = compound.getUniqueId("sim " + i);
-                EntitySim e = (EntitySim) server.getEntityFromUuid(sim);
-                if (!unemployedsims.contains(e)){
-                 unemployedsims.add(e);
-            }}
-        }
-        for (int i = unemployed_sims;i<total_sims+unemployed_sims;i++){
-            if (compound.hasKey("sims " + i)){
-                UUID sim = compound.getUniqueId("sims " + i);
-                System.out.println("sim " + sim);
-                System.out.println("sim id =" + sim + "with persistent id of " + totalsims.get(i).getPersistentID());
-                EntitySim e = (EntitySim) server.getEntityFromUuid(sim);
+        for (int i = 0;i< unemployes_sims.tagCount(); i++){
+            String uuid = unemployes_sims.getStringTagAt(i);
+            UUID sim = UUID.fromString(uuid);
+            EntitySim e =(EntitySim) server.getEntityFromUuid(sim);
+            if (!unemployedsims.contains(e)){
+            unemployedsims.add(e);
+        }}
+            for (int i = 0;i< total_sims.tagCount(); i++){
+                String uuid = unemployes_sims.getStringTagAt(i);
+                UUID sim = UUID.fromString(uuid);
+                EntitySim e =(EntitySim) server.getEntityFromUuid(sim);
                 if (!totalsims.contains(e)){
-                totalsims.add(e);
-            }
+                   totalsims.add(e);
+                }}
         }
     }
-}}
+}
+
