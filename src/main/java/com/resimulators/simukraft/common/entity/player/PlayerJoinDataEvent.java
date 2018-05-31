0380df74f28428339b46cc0a01a8ee165fca388a
aidie8 <aidenvanzuilen@gmail.com>
2018-05-31 19:16:47 +1200

    fixes

--- src/main/java/com/resimulators/simukraft/common/entity/player/PlayerJoinDataEvent.java
+++ src/main/java/com/resimulators/simukraft/common/entity/player/PlayerJoinDataEvent.java
@@ -6,14 +6,14 @@ import com.resimulators.simukraft.network.PlayerUpdatePacket;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
 public class PlayerJoinDataEvent {
 
     @SubscribeEvent
-    public static void Player_Join(EntityJoinWorldEvent event) {
-        if (event.getEntity() instanceof EntityPlayerMP){
-        if (!event.getWorld().isRemote) {
+    public static void Player_Join(PlayerEvent.PlayerLoggedInEvent event) {
+        if (!event.player.world.isRemote) {
             PacketHandler.INSTANCE.sendTo(new PlayerUpdatePacket(SimEventHandler.getWorldSimData().getTotalSims(), SimEventHandler.getWorldSimData().getUnemployed_sims(), SimEventHandler.getCredits()), (EntityPlayerMP) event.getEntity());
             System.out.println("Data sent");
         }}
-    }}
+    }
