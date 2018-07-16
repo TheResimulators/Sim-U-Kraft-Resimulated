b2762080b1242a944e7aa8fb308d698a9a9ec0c9
aidie8 <aidenvanzuilen@gmail.com>
2018-07-16 15:20:13 +1200

    Fixed TileFarm desync

--- src/main/java/com/resimulators/simukraft/common/tileentity/TileFarm.java
+++ src/main/java/com/resimulators/simukraft/common/tileentity/TileFarm.java
@@ -7,6 +7,8 @@ import com.resimulators.simukraft.network.GetSimIdPacket;
 import com.resimulators.simukraft.network.PacketHandler;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
@@ -139,5 +141,22 @@ public class TileFarm extends TileEntity implements ITickable,ISimJob {
 
     public void setSeed(int seed) {
         this.seed = seed;
+    }
+
+    @Override
+    public NBTTagCompound getUpdateTag() {
+        return writeToNBT(new NBTTagCompound());
+    }
+
+    @Override
+    public SPacketUpdateTileEntity getUpdatePacket() {
+        return new SPacketUpdateTileEntity(this.pos,1,writeToNBT(getUpdateTag()));
+    }
+
+
+    @Override
+    public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity packet) {
+        readFromNBT(packet.getNbtCompound());
+
     }
 }
