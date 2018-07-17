95a253a35f0ae630c5435ae7932e1fe6561c12c7
aidie8 <aidenvanzuilen@gmail.com>
2018-07-17 23:52:12 +1200

    added Control-Box TileEntity

--- src/main/java/com/resimulators/simukraft/common/block/BlockControlBox.java
+++ src/main/java/com/resimulators/simukraft/common/block/BlockControlBox.java
@@ -1,11 +1,14 @@
 package com.resimulators.simukraft.common.block;
 
 import com.resimulators.simukraft.common.block.base.BlockBase;
+import com.resimulators.simukraft.common.tileentity.TileController;
+import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
@@ -16,7 +19,7 @@ import java.util.Random;
 /**
  * Created by fabbe on 15/01/2018 - 8:47 PM.
  */
-public class BlockControlBox extends BlockBase {
+public class BlockControlBox extends BlockBase implements ITileEntityProvider {
     public BlockControlBox(String name, CreativeTabs tab, Material blockMaterialIn, MapColor blockMapColorIn) {
         super(name, tab, blockMaterialIn, blockMapColorIn);
     }
@@ -30,5 +33,10 @@ public class BlockControlBox extends BlockBase {
     @Override
     public int quantityDropped(Random random) {
         return 0;
+    }
+
+    @Override
+    public TileEntity createNewTileEntity(World world, int i) {
+        return new TileController();
     }
 }
---
+++ src/main/java/com/resimulators/simukraft/common/tileentity/TileController.java
@@ -0,0 +1,85 @@
+package com.resimulators.simukraft.common.tileentity;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
+import net.minecraft.tileentity.TileEntity;
+
+public class TileController extends TileEntity {
+    private boolean occupied;
+    private String type;
+    private float rent;
+    private int[] simMarker;
+    TileController(){}
+
+
+    public void setOccupied(boolean occupied){
+        this.occupied = occupied;
+    }
+
+    public boolean isOccupied(){
+        return occupied;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public void setRent(float rent) {
+        this.rent = rent;
+    }
+
+    public float getRent(){
+        return rent;
+    }
+
+    public void setSimMarker(int[] simMarker){
+        this.simMarker = simMarker;
+    }
+
+    public int[] getSimMarker(){
+        return simMarker;
+    }
+
+
+    @Override
+    public void readFromNBT(NBTTagCompound nbt) {
+        super.readFromNBT(nbt);
+        setOccupied(nbt.getBoolean("occupied"));
+        setType(nbt.getString("type"));
+        setRent(nbt.getFloat("rent"));
+        setSimMarker(nbt.getIntArray("simmarker"));
+    }
+
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound nbt){
+        super.writeToNBT(nbt);
+        nbt.setBoolean("occupied", occupied);
+        nbt.setString("type",type);
+        nbt.setFloat("rent",rent);
+        nbt.setIntArray("simmarker",simMarker);
+        return nbt;
+    }
+
+    @Override
+    public NBTTagCompound getUpdateTag() {
+        return writeToNBT(new NBTTagCompound());
+    }
+
+    @Override
+    public SPacketUpdateTileEntity getUpdatePacket() {
+        return new SPacketUpdateTileEntity(this.pos,2,writeToNBT(getUpdateTag()));
+    }
+
+
+    @Override
+    public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity packet) {
+        readFromNBT(packet.getNbtCompound());
+
+    }
+}
