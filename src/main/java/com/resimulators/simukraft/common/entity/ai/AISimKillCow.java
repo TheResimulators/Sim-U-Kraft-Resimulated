7c67e020f0d439d41d9ee5bfbaceb6d36cd56e93
aidie8 <aidenvanzuilen@gmail.com>
2018-09-04 21:02:19 +1200

    Added Kill Cow AI. W.I.P

--- src/main/java/com/resimulators/simukraft/common/entity/ai/AISimKillCow.java
+++ src/main/java/com/resimulators/simukraft/common/entity/ai/AISimKillCow.java
@@ -2,6 +2,8 @@ package com.resimulators.simukraft.common.entity.ai;
 
 import com.resimulators.simukraft.common.entity.entitysim.EntitySim;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.ai.EntityAIAttackMelee;
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.util.DamageSource;
@@ -14,25 +16,28 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class AISimKillCow extends EntityAIBase {
+public class AISimKillCow extends EntityAIAttackMelee {
     private int counter = 0;
     private EntitySim sim;
     private World world = FMLCommonHandler.instance().getMinecraftServerInstance().getEntityWorld();
     private Random rnd = new Random();
     private boolean killing = false;
-    public AISimKillCow(EntitySim sim ){
+    public AISimKillCow(double speed, boolean memory,EntitySim sim){
+        super(sim,speed,memory);
         this.sim = sim;
+        setMutexBits(7);
     }
     @Override
     public boolean shouldExecute() {
-        if (counter/20 == 5 && !killing){
+        if (sim.getJobBlockPos() != null){
+        if (counter/20 == 5 && !killing && sim.getLabeledProfession().equals("Cattle Farmer") && sim.getDistanceSq(sim.getJobBlockPos()) > 2){
             killing = true;
-            return true;
+            return super.shouldExecute();
         }else{
             counter++;
             return false;
         }
-    }
+    }else {return false;}}
 
 
     @Override
@@ -45,11 +50,15 @@ public class AISimKillCow extends EntityAIBase {
             }
             System.out.println("is this working");
             EntityCow cow = cows.get(rnd.nextInt(cows.size()-1));
-            sim.faceEntity(cow,0,0);
-            cow.attackEntityFrom(DamageSource.causeMobDamage(sim),5);
-            sim.swingArm(EnumHand.MAIN_HAND);
+            sim.setAttackTarget(cow);
+            super.startExecuting();
 
         }
+    }
+
+    @Override
+    public void resetTask(){
+        killing = false;
     }
 
 }
