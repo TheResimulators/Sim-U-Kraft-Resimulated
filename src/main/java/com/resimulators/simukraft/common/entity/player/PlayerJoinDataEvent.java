a6ce323e818f881775b5f373cb1b3424fc4fc7e1
aidie8 <aidenvanzuilen@gmail.com>
2019-01-13 10:36:08 +1300

    removed spam

--- src/main/java/com/resimulators/simukraft/common/entity/player/PlayerJoinDataEvent.java
+++ src/main/java/com/resimulators/simukraft/common/entity/player/PlayerJoinDataEvent.java
@@ -27,14 +27,10 @@ public class PlayerJoinDataEvent {
         if (!event.player.world.isRemote) {
             Random rnd = new Random();
             if (event.player.hasCapability(ModCapabilities.PlayerCap,null)){
-                System.out.println(event.player.getCapability(ModCapabilities.PlayerCap, null));
             Long factionid = event.player.getCapability(ModCapabilities.PlayerCap,null).getfactionid();
-            System.out.println("faction id " + factionid);
             if (factionid == 0){
                 long id = rnd.nextLong();
-                System.out.println("random long value "+ id);
                 event.player.getCapability(ModCapabilities.PlayerCap,null).setfaction(id);
-                System.out.println("after setting factionid "+ event.player.getCapability(ModCapabilities.PlayerCap,null).getfactionid());
                 SaveSimData.get(event.player.world).addfaction(id,event.player.getName());
                 SaveSimData.get(event.player.world).getfaction(id).addPlayer(event.player.getUniqueID());
 
@@ -44,12 +40,6 @@ public class PlayerJoinDataEvent {
             if (SaveSimData.get(event.player.world) == null)
                 return;
                 event.player.getCapability(ModCapabilities.PlayerCap,null).updateClient(event.player);
-                System.out.println(SaveSimData.get(event.player.world).getFactions());
-                System.out.println(SaveSimData.get(event.player.world).getfaction(factionid).getTotalSims());
-                System.out.println(SaveSimData.get(event.player.world).getfaction(factionid).getUnemployedSims());
-                System.out.println(SimEventHandler.getCredits());
-                System.out.println(event.player.getCapability(ModCapabilities.PlayerCap,null).getmode());
-                System.out.println(factionid);
                 PacketHandler.INSTANCE.sendTo(new SaveSimDataUpdatePacket(SaveSimData.get(event.player.world).serializeNBT()),(EntityPlayerMP) event.player);
             PacketHandler.INSTANCE.sendTo(new PlayerUpdatePacket(SaveSimData.get(event.player.world).getfaction(factionid).getTotalSims(), SaveSimData.get(event.player.world).getfaction(factionid).getUnemployedSims(), SimEventHandler.getCredits(),factionid,event.player.getCapability(ModCapabilities.PlayerCap,null).getmode()), (EntityPlayerMP) event.player);
         }
